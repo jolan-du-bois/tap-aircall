b9ddc37dbdf3a7850ec6af779ed00974437e0c1f
Alexandre Brehelin <alexandre.brehelin@needelp.com>
2022-08-17 08:35:45 +0200

    cookiecutter tap-aircall

---
+++ tap_aircall/auth.py
@@ -0,0 +1,31 @@
+"""aircall Authentication."""
+
+
+from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
+
+
+# The SingletonMeta metaclass makes your streams reuse the same authenticator instance.
+# If this behaviour interferes with your use-case, you can remove the metaclass.
+class aircallAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
+    """Authenticator class for aircall."""
+
+    @property
+    def oauth_request_body(self) -> dict:
+        """Define the OAuth request body for the aircall API."""
+        # TODO: Define the request body needed for the API.
+        return {
+            'resource': 'https://analysis.windows.net/powerbi/api',
+            'scope': self.oauth_scopes,
+            'client_id': self.config["client_id"],
+            'username': self.config["username"],
+            'password': self.config["password"],
+            'grant_type': 'password',
+        }
+
+    @classmethod
+    def create_for_stream(cls, stream) -> "aircallAuthenticator":
+        return cls(
+            stream=stream,
+            auth_endpoint="TODO: OAuth Endpoint URL",
+            oauth_scopes="TODO: OAuth Scopes",
+        )
