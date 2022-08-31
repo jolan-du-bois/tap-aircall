a2c30d80371f974770e53d651d265e86667b7494
Alexandre Brehelin <alexandre.brehelin@needelp.com>
2022-08-31 21:17:24 +0200

    [shell script] run exemple with catalog & state

---
+++ tap-aircall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+mode="$1"
+timestamp=$(date +%s)
+
+poetry install tap-aircall
+
+if [ "$mode" = "catalog" ]
+then
+  echo "Mode $mode: running without state"
+  poetry run tap-aircall \
+    --config .secrets/config.json \
+    --catalog samples/catalog.json >> output/output_${timestamp}.json
+  tail -1 output/output_${timestamp}.json > samples/state.json.tmp && mv samples/state.json.tmp samples/state.json
+  cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
+elif [ "$mode" = "state" ]
+then
+  echo "Mode $mode: running with state config from sample"
+  poetry run tap-aircall \
+  --config .secrets/config.json \
+  --catalog samples/catalog.json \
+  --state samples/state.json >> output/output_${timestamp}.json
+  cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
+else
+    echo "Mode $mode: running nothing"
+fi
