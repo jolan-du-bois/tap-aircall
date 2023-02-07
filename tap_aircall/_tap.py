6086553d7186cbd3799b30e5f7052f953e4a6c8f
vorpal56 <anthonysukadil1@gmail.com>
2023-02-07 14:14:54 -0500

    Include --properties arg and move click.options to variables, update docs

--- tap_aircall/_tap.py
+++ tap_aircall/_tap.py
@@ -8,10 +8,52 @@ from singer_sdk.helpers._classproperty import classproperty
 from singer_sdk.tap_base import CliTestOptionValue
 from singer_sdk.cli import common_options
 
+CONFIG_OPTION = click.option(
+    "-c",
+    multiple=True,
+    help="Configuration file location or 'ENV' to use environment variables.",
+    type=click.STRING,
+    default=(),
+)
+DISCOVER_OPTION = click.option(
+    "--discover",
+    is_flag=True,
+    help="Run the tap in discovery mode.",
+)
+TEST_OPTION = click.option(
+    "--test",
+    is_flag=False,
+    flag_value=CliTestOptionValue.All.value,
+    default=CliTestOptionValue.Disabled,
+    help=(
+        "Use --test to sync a single record for each stream. "
+        + "Use --test=schema to test schema output without syncing "
+        + "records."
+    ),
+)
+CATALOG_OPTION = click.option(
+    "--catalog",
+    help="Use a Singer catalog file with the tap.",
+    type=click.Path(),
+)
+PROPERTIES_OPTION = click.option(
+    "--properties",
+    help="Use a Singer catalog file with the tap.",
+    type=click.Path(),
+)
+STATE_OPTION = click.option(
+    "--state",
+    help="Use a bookmarks file for incremental replication.",
+    type=click.Path(),
+)
+
 class _Tap(Tap):
     """
-    Monkeypatches the Tap.cli to accept -c arg as the same as --config
-    If both are provided, --config arg is accepted first
+    Monkeypatches the Tap.cli to accept:
+    1. -c arg as the same as --config
+        - If both are provided, --config arg is accepted first
+    2. --properties arg as the same as --catalog, but deprecated
+        - If both are provided, --catalog arg is accepted first
     """
     @classproperty
     def cli(cls) -> Callable:
@@ -25,39 +67,12 @@ class _Tap(Tap):
         @common_options.PLUGIN_ABOUT
         @common_options.PLUGIN_ABOUT_FORMAT
         @common_options.PLUGIN_CONFIG
-        @click.option(
-            "-c",
-            multiple=True,
-            help="Configuration file location or 'ENV' to use environment variables.",
-            type=click.STRING,
-            default=(),
-        )
-        @click.option(
-            "--discover",
-            is_flag=True,
-            help="Run the tap in discovery mode.",
-        )
-        @click.option(
-            "--test",
-            is_flag=False,
-            flag_value=CliTestOptionValue.All.value,
-            default=CliTestOptionValue.Disabled,
-            help=(
-                "Use --test to sync a single record for each stream. "
-                + "Use --test=schema to test schema output without syncing "
-                + "records."
-            ),
-        )
-        @click.option(
-            "--catalog",
-            help="Use a Singer catalog file with the tap.",
-            type=click.Path(),
-        )
-        @click.option(
-            "--state",
-            help="Use a bookmarks file for incremental replication.",
-            type=click.Path(),
-        )
+        @CONFIG_OPTION
+        @DISCOVER_OPTION
+        @TEST_OPTION
+        @CATALOG_OPTION
+        @PROPERTIES_OPTION
+        @STATE_OPTION
         @click.command(
             help="Execute the Singer tap.",
             context_settings={"help_option_names": ["--help"]},
@@ -71,6 +86,7 @@ class _Tap(Tap):
             c: Tuple[str, ...] = (),
             state: str = None,
             catalog: str = None,
+            properties: str = None,
             format: str = None,
         ) -> None:
             """Handle command line execution.
@@ -83,7 +99,9 @@ class _Tap(Tap):
                 format: Specify output style for `--about`.
                 config: Configuration file location or 'ENV' to use environment
                     variables. Accepts multiple inputs as a tuple.
-                catalog: Use a Singer catalog file with the tap.",
+                c: Same as `config`
+                catalog: Use a Singer catalog file with the tap.
+                properties: Same as, but deprecated in favour of `catalog`.
                 state: Use a bookmarks file for incremental replication.
 
             Raises:
@@ -126,7 +144,7 @@ class _Tap(Tap):
             tap = cls(  # type: ignore  # Ignore 'type not callable'
                 config=config_files or None,
                 state=state,
-                catalog=catalog,
+                catalog=catalog or properties,
                 parse_env_config=parse_env_config,
                 validate_config=validate_config,
             )
