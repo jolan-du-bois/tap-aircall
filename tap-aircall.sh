2ea25545c0c34b005bf1113faffce6b6b5e0acf1
Jolan DU BOIS <jolan@needelp.com>
2023-08-04 17:30:42 +0200

    fix: typos

--- tap-aircall.sh
+++ tap-aircall.sh
@@ -10,14 +10,14 @@ then
   echo "Mode $mode: running without state"
   poetry run tap-aircall \
     --config .secrets/config.json >> output/output_${timestamp}.json
-  tail -1 output/output_${timestamp}.json > samples/state.json.tmp && mv samples/state.json.tmp samples/state.json
+  tail -1 output/output_${timestamp}.json >> samples/state.json.tmp && mv samples/state.json.tmp samples/state.json
   cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
 elif [ "$mode" = "state" ]
 then
   echo "Mode $mode: running with state config from sample"
   poetry run tap-aircall \
   --config .secrets/config.json \
-  --state samples/state.jso./tn >> output/output_${timestamp}.json
+  --state samples/state.json >> output/output_${timestamp}.json
   cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
 else
     echo "Mode $mode: running nothing"
