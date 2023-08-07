ecca2ee73fa2dc0973519f13ba6f63146f51ca91
Jolan DU BOIS <jolan@needelp.com>
2023-08-07 13:52:59 +0200

    fix: removed the --about (it was already implemented)

--- tap-aircall.sh
+++ tap-aircall.sh
@@ -26,9 +26,10 @@
 mode="$1"
 timestamp=$(date +%s)
 
+poetry install
+
 if [ "$mode" = "catalog" ]
 then
-  poetry install
   echo "Mode $mode: running without state"
   poetry run tap-aircall \
     --config .secrets/config.json >> output/output_${timestamp}.json
@@ -37,17 +38,11 @@ then
   cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
 elif [ "$mode" = "state" ]
 then
-  poetry install
   echo "Mode $mode: running with state config from sample"
   poetry run tap-aircall \
   --config .secrets/config.json \
   --state samples/state.json >> output/output_${timestamp}.json
   cat output/output_${timestamp}.json | grep RECORD >> output/output_record_${timestamp}.json
-elif [ "$mode" = "--about" ]
-then 
-  echo "Options and arguments :"
-  echo "  catalog : retrieves all data starting from the date specified in .secrets/config.json"
-  echo "  state : retrieves all data starting from the state indicated in samples/state.json"
 else
   echo "Mode $mode: running nothing"
 fi
